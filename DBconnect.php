784af5b56e6d5825d73e7439db0e61b03004ecd8
Julio Gonzales <dg2007xd@gmail.com>
2025-06-05 11:53:48 -0500

    other host

--- DBconnect.php
+++ DBconnect.php
@@ -1,3 +1,4 @@
 <?php
-$cn = new PDO("mysql:host=localhost;dbname=gym", "root", "");
+//$cn = new PDO("mysql:host=localhost;dbname=gym", "root", "");
+$cn = new PDO("mysql:host=mysql-dg2007xd.alwaysdata.net;dbname=dg2007xd_wbgym", "dg2007xd", "wbgym.2025");
 ?>
